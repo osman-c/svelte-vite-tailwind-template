ea1c9c7c9605263f0ee53267d45c6e0cc4cf3215
Osmancan Özkan <56242266+osman-c@users.noreply.github.com>
2022-04-23 21:54:42 +0300

    second first commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
+  import TailwindCss from "./TailwindCss.svelte";
+</script>
+
+<TailwindCss />
+<nav class="bg">??????</nav>
